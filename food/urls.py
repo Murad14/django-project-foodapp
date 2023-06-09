ea8bd5b316211f2a581311122fdedcc244b04be0
Murad <49407545+Murad14@users.noreply.github.com>
2023-06-10 02:44:34 +0600

    class based views implemented

--- food/urls.py
+++ food/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 app_name = 'food'
 urlpatterns = [
     #/food/
-    path('', views.index, name= 'index'),
+    path('', views.IndexClassView.as_view(), name= 'index'),
     #/food/1
-    path('<int:item_id>/',views.detail,name='detail'),
+    path('<int:pk>/',views.FoodDetailView.as_view(),name='detail'),
     path ('item/', views.item,name='item'),
     #add items
     path('add',views.create_item, name='create_item'),
--- food/views.py
+++ food/views.py
@@ -3,18 +3,27 @@ from django.http import HttpResponse
 from .models import Item
 from django.template import loader
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
-def index(req):
-    item_list = Item.objects.all()
-    # template = loader.get_template('food/index.html')
-    context = {
-        'item_list': item_list,
-    }
-    # return HttpResponse(template.render(context,req))
-    # Alternative way below
-    return render(req,'food/index.html',context)
+# def index(req):
+#     item_list = Item.objects.all()
+#     # template = loader.get_template('food/index.html')
+#     context = {
+#         'item_list': item_list,
+#     }
+#     # return HttpResponse(template.render(context,req))
+#     # Alternative way below
+#     return render(req,'food/index.html',context)
+
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
+
 
 
 
@@ -22,12 +31,18 @@ def index(req):
 def item(req):
     return HttpResponse('<h1>This is an item view</h1>')
 
-def detail(req,item_id):
-    item = Item.objects.get(pk=item_id)
-    context = {
-        'item':item
-    }
-    return render(req,'food/detail.html',context)
+# def detail(req,item_id):
+#     item = Item.objects.get(pk=item_id)
+#     context = {
+#         'item':item
+#     }
+#     return render(req,'food/detail.html',context)
+
+class FoodDetailView(DetailView):
+    model = Item
+    template_name = "food/detail.html"
+    
+
 
 def create_item(req):
     form = ItemForm(req.POST or None)
